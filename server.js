1d7ca7d81da9684b1e750cda901ff18ea404a4cb
Kyem Asa <turtlemoons@protonmail.com>
2023-01-02 18:52:34 -0600

    connect db

--- server.js
+++ server.js
@@ -7,6 +7,17 @@ require('dotenv').config()
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
 
+let db,
+    dbConnectionStr = process.env.DB_STRING,
+    dbName = 'webtoons'
+
+MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true})
+    .then(client => {
+        console.log(`Connected to ${dbName} Database`)
+        db = client.db(dbName)
+    })
+
+    
 
 app.listen(3000, () => {
     console.log(`listening on port ${PORT}`)
