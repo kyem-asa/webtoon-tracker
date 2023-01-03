284addc129265b7a3b719d7355fcdeb651c8f590
Kyem Asa <turtlemoons@protonmail.com>
2023-01-02 18:54:52 -0600

    add app  use methods

--- server.js
+++ server.js
@@ -17,7 +17,13 @@ MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true})
         db = client.db(dbName)
     })
 
-    
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
+
 
 app.listen(3000, () => {
     console.log(`listening on port ${PORT}`)
